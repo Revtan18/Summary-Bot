01c9d9c7050ff2e25278d05ce79a1df6eb768050
Amerohful <semb1996@gmail.com>
2019-09-22 06:43:41 +0300

    Was added question about name

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot import TeleBot
+import config
 
-bot = TeleBot("922442502:AAGH0X59pX7HSjH7XJD-uJTdc_2fO9Ptdbs")
+bot = TeleBot(config.token)
+
+user_dict = {}
+
+
+class User:
+
+    def __init__(self, name):
+        self.name = name
 
 
 @bot.message_handler(commands=['start'])
@@ -14,17 +23,30 @@ def start_message(message):
     
     keyboard.add (yes_bottom, no_bottom,)
     keyboard.add (some_bottom) 
+
     bot.send_message(message.chat.id, 'Ну здравствуй! Лампа не сильно в глаза светит? Наручники не трут? Ну ничего, привыкнешь.\n Я бот - специалист по извлечению резюме из таких как ты. И вопросы здесь задаю я. \n Это понятно?', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data in ('a', 'b', 'c'))
 def callback_inline(call):
     if call.data == 'a':
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Отлично! Тогда давай приступим', reply_markup=None)
+        msg = bot.reply_to(call.message, 'Отлично! Тогда давай приступим, как тебя зовут?')
     elif call.data == 'b':
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Ну что ж. Значит, сейчас разберешься', reply_markup=None)
+        msg = bot.reply_to(call.message, 'Ну чтож, значит сейчас разберешься. Как тебя зовут?')
     else:
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Ну все, развезло. А я еще только начал. Вот, держи водички. А я перейду к вопросам.', reply_markup=None)
+        msg = bot.reply_to(call.message, 'Ну все, развезло. А я еще только начал. Вот, держи водички. А я перейду к вопросам. Как тебя зовут?')
+    bot.register_next_step_handler(msg, process_name_step)
+
+
+def process_name_step(message):
+    try:
+        chat_id = message.chat.id
+        name = message.text
+        user = User(name)
+        user_dict[chat_id] = user
+        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\nGrats!')
+    except Exception as e:
+        bot.reply_to(message, 'oops')
 	
 
 
